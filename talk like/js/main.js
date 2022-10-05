8107a7d86dcaf99928d0aff28c113c6bcc26b84c
takushikai <takushikai@gmail.com>
2022-10-05 22:49:27 +0900

    commonに移動

--- talk like/js/main.js
+++ talk like/js/main.js
@@ -1,10 +1,26 @@
-tippy("#image_left", {
+const left_tippy = tippy("#image_left", {
     content: "<h1>I'm left</h1>",
     allowHTML: true,
-});
+    theme: "left",
+    onHide(instance) {
+        return false;
+    },
+})[0];
 
-tippy("#image_right", {
+const right_tippy = tippy("#image_right", {
     content: "<h1>I'm right</h1>",
     allowHTML: true,
     backgroundColor: "yellow",
-});
+    theme: "right",
+    onHide(instance) {
+        return false;
+    },
+})[0];
+
+// console.log(left_tippy);
+
+//常時表示は、show()とfn onHide:return false
+left_tippy.show();
+right_tippy.show();
+
+// left_tippy.setContent("string");
