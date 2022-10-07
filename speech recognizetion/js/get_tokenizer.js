66b6dd0cf2a68681ed478a99d6383764c15a8e82
takushikai <takushikai@gmail.com>
2022-10-07 12:57:04 +0900

    kuromojiを追加

---
+++ speech recognizetion/js/get_tokenizer.js
@@ -0,0 +1,12 @@
+function get_tokenizer(){
+    return new Promise(resolve=>{
+        kuromoji.builder({ dicPath: "./lib/kuromoji/dict" })
+        .build((err, tokenizer) => {
+            resolve(tokenizer);
+        });
+    });
+}
+
+const tokenizer = await get_tokenizer();
+
+export default tokenizer;
