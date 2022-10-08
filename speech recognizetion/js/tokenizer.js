4a73d45459f1a4ecc5211400b89e6045cfa105df
takushikai <takushikai@gmail.com>
2022-10-08 18:20:17 +0900

    htmlと同じ階層に、kuromoji_dict_path.txtの設置をする方式に。

--- speech recognizetion/js/tokenizer.js
+++ speech recognizetion/js/tokenizer.js
@@ -1,11 +1,32 @@
 import {kuromoji} from "../lib/kuromoji/kuromoji-esm.js";
+//htmlと同じ階層に、kuromoji_dict_path.txtを設置する必要あり
 
 function get_tokenizer(){
-    return new Promise(resolve=>{
-        kuromoji.builder({ dicPath: "../../speech recognizetion/lib/kuromoji/dict" })
+    return new Promise(async resolve =>{
+        kuromoji.builder({ dicPath:  await get_dict_path()})
         .build((err, tokenizer) => {
             resolve(tokenizer);
         });
+    });
+}
+
+async function get_dict_path(){
+    const regexp = new RegExp(".*/(.*?)$");
+
+    return await getDataAsString(location.href.match(regexp)[0] + "/kuromoji_dict_path.txt");
+}
+
+async function getDataAsString(path){
+    return new Promise(resolve=>{
+        const xhr = new XMLHttpRequest();
+
+        xhr.open("get", path);
+        xhr.send();
+        xhr.onreadystatechange = function() {
+            if( xhr.readyState === 4 && xhr.status === 200) {
+                resolve(this.responseText);
+            }
+        }
     });
 }
 
