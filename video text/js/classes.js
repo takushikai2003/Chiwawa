7dcf640094cc9b83128efec7109714fec39101a1
takushikai <takushikai@gmail.com>
2022-10-08 21:48:59 +0900

    video textをクラス化

---
+++ video text/js/classes.js
@@ -0,0 +1,117 @@
+const video_element = document.getElementById("video_element");
+let subtitle_data = [];
+let text_replaced = false;
+
+class video_text{
+    constructor({video_src, text_data_src}){
+
+        video_element.src = video_src;
+        // video_element.pause();
+        
+        async function getDataAsString(path){
+            return new Promise(resolve=>{
+                const xhr = new XMLHttpRequest();
+        
+                xhr.open("get", path);
+                xhr.send();
+                xhr.onreadystatechange = function() {
+                    if( xhr.readyState === 4 && xhr.status === 200) {
+                        resolve(this.responseText);
+                    }
+                }
+            });
+        }
+        
+        getDataAsString(text_data_src)
+        .then(data=>{
+            subtitle_data = JSON.parse(data);
+        });
+       
+        let _resolve;
+        this.onend = function(){
+            return new Promise(resolve=>{
+                _resolve = resolve;
+            });
+        }
+
+        video_element.addEventListener("ended",()=>{
+            _resolve();
+        },{once: true});
+    }
+}
+
+let subtitle_data_copy = [];
+video_element.addEventListener("play", ()=>{
+    text_replaced = false;
+    subtitle_data_copy = Array.from(subtitle_data);
+});
+
+
+video_element.addEventListener("timeupdate", ()=>{
+    const currentTime = video_element.currentTime;
+    if(subtitle_data_copy.length == 0){
+        return;
+    }
+
+    if(
+        subtitle_data_copy[0].startTime <= currentTime &&
+        currentTime <= subtitle_data_copy[0].endTime &&
+        !text_replaced
+    ){
+        replace_text(subtitle_data_copy[0].data);
+        text_replaced = true;
+        return;
+    }
+
+    if(currentTime >= subtitle_data_copy[0].endTime){
+        subtitle_data_copy.shift();
+        document.getElementById("text_area").innerHTML = "";
+        text_replaced = false;
+        return;
+    }
+});
+
+
+let old_tippy = tippy(".meaning_word", {
+    onTrigger: (instance, event) => {
+        instance.setContent(instance.reference.dataset.tip);
+    },
+    allowHTML: true,
+    placement: "bottom",
+});
+
+function replace_text(data){
+    const text_area = document.getElementById("text_area");
+    text_area.innerHTML = "";
+
+    for(let i=0; i<old_tippy.length; i++){
+        old_tippy[i].destroy();
+    }
+
+    for(let i=0; i<data.length; i++){
+        const text = data[i].text;
+        const tip = data[i].tip;
+
+        const span = document.createElement("span");
+        span.innerHTML = text;
+
+        if(tip.length != 0 || tip == undefined){
+            span.setAttribute("class", "meaning_word");
+            span.dataset.tip = tip;
+        }
+
+        text_area.appendChild(span);
+    }
+
+
+    old_tippy = tippy(".meaning_word", {
+        onTrigger: (instance, event) => {
+            instance.setContent(instance.reference.dataset.tip);
+        },
+        allowHTML: true,
+        placement: "bottom",
+    });
+}
+
+
+export {video_text};
