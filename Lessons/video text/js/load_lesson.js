93cd409fbb5bf4adc70a25911078c2db862ce2a3
takushikai <takushikai@gmail.com>
2022-10-23 18:19:03 +0900

    fix

--- Lessons/video text/js/load_lesson.js
+++ Lessons/video text/js/load_lesson.js
@@ -5,7 +5,7 @@ const url = new URL(window.location.href);
 const params = url.searchParams;
 const lesson_nuber = params.get("lesson");
 
-const data = JSON.parse(await getDataAsString("../lessons_data/lessons/lesson${lesson_nuber}_video.json"));
+const data = JSON.parse(await getDataAsString(`../lessons_data/lessons/lesson${lesson_nuber}_video.json`));
 
 const vt1 = new video_text(data);
 await vt1.onend();
