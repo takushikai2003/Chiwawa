30043905373cf778ffd3516cb5afd266c1dc1277
takushikai <takushikai@gmail.com>
2022-10-25 09:31:08 +0900

    xmlhttprequestをfetchに変更

--- common esm/getDataAsString.js
+++ common esm/getDataAsString.js
@@ -1,14 +1,10 @@
 async function getDataAsString(path){
     return new Promise(resolve=>{
-        const xhr = new XMLHttpRequest();
-
-        xhr.open("get", path);
-        xhr.send();
-        xhr.onreadystatechange = function() {
-            if( xhr.readyState === 4 && xhr.status === 200) {
-                resolve(this.responseText);
-            }
-        }
+        fetch(path)
+        .then((response) => response.text())
+        .then((text) => {
+            resolve(text);
+        });
     });
 }
 
