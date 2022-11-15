29719bbb163acfd4e032bb903b37ab8d74f05c4e
takushikai <takushikai@gmail.com>
2022-11-15 19:30:56 +0900

    JSベース化完了

--- Lessons/video text/js/load_lesson.js
+++ Lessons/video text/js/load_lesson.js
@@ -1,4 +1,4 @@
-import { video_text } from "./classes.js";
+import { VideoText } from "./VideoText.js";
 import getDataAsString from "../../../common esm/getDataAsString.js";
 
 const url = new URL(window.location.href);
@@ -7,8 +7,8 @@ const lesson_nuber = params.get("lesson");
 
 const data = JSON.parse(await getDataAsString(`../lessons_data/lessons/lesson${lesson_nuber}_video.json`));
 
-const vt1 = new video_text(data);
+const vt1 = new VideoText(document.body, data);
 await vt1.onend();
 
 
-location.href = "../../../boot/index.html?page=lesson_select";
+// location.href = "../../../boot/index.html?page=lesson_select";
