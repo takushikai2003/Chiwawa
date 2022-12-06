b3b9c98f6593adffac32aa82f8a26c295e14d7b7
takushikai <takushikai@gmail.com>
2022-12-06 15:50:01 +0900

    add all clear

--- pages/boot/Setting.js
+++ pages/boot/Setting.js
@@ -1,6 +1,7 @@
 import getDataAsString from "../../common esm/getDataAsString.js";
 const insertHTML = await getDataAsString("./pages/boot/Setting.html");
 
+
 class Setting{
     constructor(){
         document.body.innerHTML = insertHTML;
@@ -23,6 +24,21 @@ class Setting{
         .addEventListener("click",()=>{
             if(window.confirm("Are you sure to delete score?")){
                 localStorage.setItem("score", "0");
+            }
+        });
+
+        document.querySelector("#delete_all")
+        .addEventListener("click",()=>{
+            if(window.confirm("Are you sure to delete all?")){
+                localStorage.clear();
+
+                window.indexedDB.databases().then((r) => {
+                    for (let i = 0; i < r.length; i++) {
+                        window.indexedDB.deleteDatabase(r[i].name);
+                    }
+                }).then(() => {
+                    alert("All data cleared.");
+                });
             }
         });
 
