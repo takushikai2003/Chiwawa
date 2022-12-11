ce6ed0aac2675692c398b4fdf15220358707cbf1
takushikai <takushikai@gmail.com>
2022-12-12 01:09:10 +0900

    fix recognizer

--- common esm/speech recognizetion/SpeechRecognizer.js
+++ common esm/speech recognizetion/SpeechRecognizer.js
@@ -2,7 +2,7 @@ import SpeechRecognition from "./js/SpeechRecognizerAPI.js";
 import FuzzySet from "./lib/FuzzySet/FuzzySet.esm.js";
 import get_kana from "./js/get_kana.js";
 
-//arg seek_message: string
+//arg search_message: string
 const SpeechRecognizer = {
     start: start,
     stop: stop,
@@ -17,25 +17,34 @@ function start(){
 }
 
 //return: bool
-function stop(seek_message){
+function stop(search_message){
     return new Promise(async resolve=>{
         const message = await SpeechRecognition.stop();
         
-        const result = judge(message, get_kana(seek_message).join(""));//bool
+        const result = judge(message, search_message);//bool
         resolve({correct: result, message: message});
     });
 }
 
 
 
-function judge(message, seek_message){
+function judge(message, search_message){
 
     const threshold = 0.8;
 
-    const message_kana = get_kana(message);
+    const search_message_kana = get_kana(search_message).join("");
+    const message_kana = get_kana(message).join("");
 
-    const fs = FuzzySet(message_kana);
-    const fs_result = fs.get(seek_message);
+    console.log("speech recognizetion result:",
+    {
+        recgnized: message,
+        search: search_message,
+        recgnized_kana: message_kana,
+        search_kana: search_message_kana
+    });
+
+    const fs = FuzzySet([message_kana]);
+    const fs_result = fs.get(search_message_kana);
 
     if(fs_result == null){
         return false;
