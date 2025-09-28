b5552277fc8750dc9fedafb91b4cbeb900f8779a
takushikai <takushikai@gmail.com>
2025-09-28 23:15:22 +0900

    kuromoji_dict_path.txtを廃止

--- common esm/speech recognizetion/js/tokenizer.js
+++ common esm/speech recognizetion/js/tokenizer.js
@@ -1,10 +1,7 @@
-import {kuromoji} from "../lib/kuromoji/kuromoji-esm.js";
-import getDataAsString from "../../getDataAsString.js";
-//htmlと同じ階層に、kuromoji_dict_path.txtを設置する必要あり
+import { kuromoji } from "../lib/kuromoji/kuromoji-esm.js";
 
 function get_tokenizer(){
     return new Promise(async resolve =>{
-        // const dict_path = await get_dict_path();
         const dict_path = get_dict_path();
         console.log(dict_path);
 
@@ -18,13 +15,6 @@ function get_tokenizer(){
 function get_dict_path(){
     return new URL("../lib/kuromoji/dict", import.meta.url).pathname;
 }
-
-// async function get_dict_path(){
-//     const regexp = new RegExp(".*/(.*?)$");
-//     const html_name = location.href.match(regexp)[1];
-//     // console.log(location.href.slice( 0, -html_name.length) + "/kuromoji_dict_path.txt");
-//     return await getDataAsString(location.href.slice( 0, -html_name.length) + "/kuromoji_dict_path.txt");
-// }
 
 const tokenizer = await get_tokenizer();
 
